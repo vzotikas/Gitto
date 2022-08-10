50f2dbbf61fef8a07bc9ac1472ce78698be0c0e5
admin <admin@home.com>
2022-08-10 22:01:31 +0200

    Initial Commit

---
+++ Gitto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gitto
+//
+//  Created by Administrator on 8/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gitto/GittoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittoApp.swift
+//  Gitto
+//
+//  Created by Administrator on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GittoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
